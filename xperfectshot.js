9108e1d2cc9812ff897f2b94258d3083a8372937
michaelkcwong <wong_michael@rogers.com>
2021-01-12 01:31:13 +0000

    adding more files

---
+++ xperfectshot.js
@@ -0,0 +1,25 @@
+//Array of moves
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+//Grid Start at 0,0
+function finalPosition(moves) {
+  let position = [0,0]
+
+  for (const move of moves) {
+    if (move === 'north') {
+      position[1] = position[1] + 1
+    }
+    else if (move === 'south') {
+      position[1] = position[1] - 1
+    }
+    else if (move === 'east') {
+      position[0] = position[0] + 1
+    }
+    else if (move === 'west') {
+      position[0] = position[0] - 1
+    }
+  }
+
+  return position 
+}
+console.log(finalPosition(moves));
